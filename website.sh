fbb9c78c5715ef5450461cae90785bfe3cfbfd51
sireereddy <117642913+sireereddy@users.noreply.github.com>
2024-07-01 16:03:17 +0530

    Update website.sh

--- website.sh
+++ website.sh
@@ -1,13 +1,16 @@
+
 #!/bin/bash
-sudo yum install wget net-tools unzip httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo apt update
+sudo apt install wget net-tools unzip figlet apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
 mkdir -p webfiles
 cd webfiles
-sudo wget https://www.tooplate.com/zip-templates/2118_chilling_cafe.zip
+sudo wget https://www.tooplate.com/zip-templates/2118_chilling_cafe.zip  
 sudo unzip -o 2118_chilling_cafe.zip
 sudo rm -rf /var/www/html/*
 sudo cp -r 2118_chilling_cafe/* /var/www/html/
 cd ..
 sudo rm -rf webfiles
-sudo systemctl restart httpd
+sudo systemctl restart apache2
+figlet done
